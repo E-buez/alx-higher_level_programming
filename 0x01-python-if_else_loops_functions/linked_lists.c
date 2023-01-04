aa8d1bde5fff2b9273ad04938b382243a3273372
E-buez <ehujuochibueze@gmail.com>
2023-01-04 15:32:13 -0800

    Task 13

--- 0x01-python-if_else_loops_functions/linked_lists.c
+++ 0x01-python-if_else_loops_functions/linked_lists.c
@@ -65,3 +65,10 @@ void free_listint(listint_t *head)
 {
     listint_t *current;
 
+    while (head != NULL)
+    {
+        current = head;
+        head = head->next;
+        free(current);
+    }
+}
