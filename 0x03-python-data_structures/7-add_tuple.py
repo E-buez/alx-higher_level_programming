1546c33ec569a646e30eba657b2342a5cf6ea918
E-buez <ehujuochibueze@gmail.com>
2023-01-08 15:08:35 -0800

    Task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
 
 
-def standardize_tuple(tup):
-    tup_len = len(tup)
-    if tup_len == 0:
-        return (0, 0)
-    elif tup_len == 1:
-        return (tup[0], 0)
-    else:
-        return (tup[0], tup[1])
-
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    sum1 = 0
-    sum2 = 0
-    tup_a_st = standardize_tuple(tuple_a)
-    tup_b_st = standardize_tuple(tuple_b)
-    sum1 = tup_a_st[0] + tup_b_st[0]
-    sum2 = tup_a_st[1] + tup_b_st[1]
+    """
+    adds the first two elements of two tuples together
+    and returns the result
+    """
+    ta_len = len(tuple_a)
+    tb_len = len(tuple_b)
+    new_tup = ()
+    for i in range(2):
+        if i >= ta_len:
+            val_a = 0
+        else:
+            val_a = tuple_a[i]
+        if i >= tb_len:
+            val_b = 0
+        else:
+            val_b = tuple_b[i]
 
-    return (sum1, sum2)
+        if (i == 0):
+            new_tup = (val_a + val_b)
+        else:
+            new_tup = (new_tup, val_a + val_b)
 
-if __name__ == '__main__':
-    tuple_a = (1, 89)
-    tuple_b = (88, 11)
-    new_tuple = add_tuple(tuple_a, tuple_b)
-    print(new_tuple)
-
-    print(add_tuple(tuple_a, (1, )))
-    print(add_tuple(tuple_a, ()))
+    return (new_tup)
