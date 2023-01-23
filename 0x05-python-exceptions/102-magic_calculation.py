f73ddebbaaa043b3d0e7030cf05a4f5afcbfc7c8
E-buez <ehujuochibueze@gmail.com>
2023-01-23 09:57:34 -0800

    Task 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 
-def magic_calculation(a, b):
+{
+        def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
@@ -13,4 +14,4 @@ def magic_calculation(a, b):
             result = b + a
             break
     return (result)
-
+}
