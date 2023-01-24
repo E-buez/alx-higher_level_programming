83472500aac8dc67cc538375eef3ffff3334df25
E-buez <ehujuochibueze@gmail.com>
2023-01-24 10:59:46 -0800

    Task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
+# 0-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
 
 
 class Square:
-    """An empty class Square."""
+    """Represent a square."""
     pass
-
-
-if __name__ == '__main__':
-    my_square = Square()
-    print(type(my_square))
-    print(my_square.__dict__)
