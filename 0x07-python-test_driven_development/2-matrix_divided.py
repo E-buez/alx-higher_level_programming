31fb2b2a563d891e0d45408c3ad0f0be4109a64a
E-buez <ehujuochibueze@gmail.com>
2023-01-30 13:23:16 -0800

    Task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -21,4 +21,3 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
     return [[round(item / div, 2) for item in lists] for lists in matrix]
-1
