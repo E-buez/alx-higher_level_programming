a1f6f27d2748621320b563e5ec3d98717d529efb
Ehujuo chibueze <ehujuochibueze@gmail.com>
2023-04-25 16:33:57 +0100

    Task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -9,4 +9,3 @@ const fileAContents = fs.readFileSync(fileA, 'utf-8');
 const fileBContents = fs.readFileSync(fileB, 'utf-8');
 
 fs.writeFileSync(process.argv[4], `${fileAContents}${fileBContents}`);
-
