7882918e083661c90b5d1c405d6cfd247019671f
Ehujuo chibueze <ehujuochibueze@gmail.com>
2023-04-26 13:37:41 +0100

    Task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
+
 """
-Script the list and filter the name the states
-"""
+ a script that takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa where name
+matches the argument.
+ """
+
 import MySQLdb
 import sys
+"""
+Import the MySQLdb and sys modules
+"""
 
 if __name__ == "__main__":
     username = sys.argv[1]
-    pas = sys.argv[2]
-    dbn = sys.argv[3]
-    statex = sys.argv[4]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
-    for i in statex:
-        if i == ';' or i == '':
-            sys.exit()
-    print(statex)
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=pas, db=dbn)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name='{:s}' ORDER BY states.id"
-                .format(statex))
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
+    db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            port=3306,
+            passwd=password,
+            db=database
+            )
+    cursor_obj = db.cursor()
+    """
+    Creates the cursor object
+    """
+    sql = ("SELECT id, name FROM states WHERE name = %s ORDER BY id  ASC")
+    cursor_obj.execute(sql, (state_name,))
+    selected_row = cursor_obj.fetchall()
+    for i in selected_row:
+        print(i)
+    cursor_obj.close()
     db.close()
