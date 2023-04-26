870c47ad920c0fc975490c02a93c2ca684d8c9f1
Ehujuo chibueze <ehujuochibueze@gmail.com>
2023-04-26 14:33:08 +0100

    Task 15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,24 +1,43 @@
 #!/usr/bin/python3
-"""
-creates the State “California” with the City “San Francisco”
-"""
-import sqlalchemy
+"""Script that creates the State “California” with the City “San Francisco”
+    """
+
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
-from sys import argv
-from relationship_city import City
 
-if __name__ == "__main__":
-    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}".format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+
+def main():
+    """Main function
+    """
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2],
+
+                                   argv[3]), pool_pre_ping=True)
+    # create all tables in the engine. This is equivalent to "Create Table"
+    Base.metadata.create_all(engine)
+    # create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+    # insantiate a Session
     session = Session()
-    s = State(name="California")
-    s.cities = [City(name="San Francisco")]
-    session.add(s)
+    # create a new State and City objects
+    state = State(name="California")
+    city = City(name="San Francisco")
+    # append the new City object to the State.cities attribute
+    state.cities.append(city)
+    # add the State object to the session
+    session.add(state)
+    # add the City object to the session
+    session.add(city)
+    # commit the session to the database
     session.commit()
+
+    # close the session
     session.close()
 
+
+if __name__ == "__main__":
+    main()
