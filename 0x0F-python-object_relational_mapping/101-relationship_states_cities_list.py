21b65e39b2af1125160307b13808956dcdd7e04c
Ehujuo chibueze <ehujuochibueze@gmail.com>
2023-04-26 14:46:53 +0100

    Task 16

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
-"""Script that prints state objects and their cities for the name passed
-    """
+"""
+lists all State objects from a database
+"""
 
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from sys import argv
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-
-
-def main():
-    # create an engine
-    engine = create_engine(
-        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}',
-        pool_pre_ping=True)
-    # Create all tables in the engine. This is equivalent to "Create Table"
-
-    Base.metadata.create_all(engine)
-
-    try:
-        with Session(bind=engine) as session:
-            # Query all State objects, and join their cities
-            states = session.query(State).order_by(State.id).all()
-            for state in states:
-                print(f'{state.id}: {state.name}')
-                for city in state.cities:
-                    print(f'\t{city.id}: {city.name}')
-    except Exception as e:
-        print(f'Something went wrong! {e}')
-    finally:
-        session.close()
-
 
 if __name__ == "__main__":
 
-    main()
-
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
+    session = Session()
+    res = session.query(State).order_by(State.id).all()
+    for state in res:
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("    {}: {}".format(city.id, city.name))
+    session.close()
