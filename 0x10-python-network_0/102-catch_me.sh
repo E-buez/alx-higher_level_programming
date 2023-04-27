3f70712eb3d729986ac9f857d9f59fcac02da905
Ehujuo chibueze <ehujuochibueze@gmail.com>
2023-04-27 17:49:22 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays the body of the response of a curl POST request
-curl -sLX PUT -d "X-School-User-Id=98" -H "Origin:You got me!" 0.0.0.0:5000/catch_me
+curl -sLX PUT -d "Origin:You got me!" 0.0.0.0:5000/catch_me
