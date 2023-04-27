d1d3a2cda76dc0bc6599fc99ce2317efeeffc1f1
Ehujuo chibueze <ehujuochibueze@gmail.com>
2023-04-27 15:09:00 +0100

    Task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sX DELETE $1 -L
+# takes in a URL and displays all HTTP methods the server will accept
+curl -ILs "$1" | grep Allow | cut -d " " -f2-
