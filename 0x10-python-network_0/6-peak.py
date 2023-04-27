3014e41bcd45a787029abec2d5e18832f5e66f00
Ehujuo chibueze <ehujuochibueze@gmail.com>
2023-04-27 16:56:49 +0100

    Task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,29 +1,13 @@
 #!/usr/bin/python3
+""" Test function find_peak """
 
 
-def find_peak(list_of_integers):
-
-    if list_of_integers is None or len(list_of_integers) == 0:
+def find_peak(lx):
+    """fynction to find peak """
+    if not lx:
         return None
+    for i in range(len(lx)):
+        if lx[0] < lx[i]:
+            lx[0] = lx[i]
 
-    if len(list_of_integers) == 1:
-        return list_of_integers[0]
-
-    mid_idx = int(len(list_of_integers) / 2)
-
-    if mid_idx != len(list_of_integers) - 1:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
-           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-    else:
-        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
-            return list_of_integers[mid_idx]
-        else:
-            return list_of_integers[mid_idx - 1]
-
-    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
-        a_list = list_of_integers[0:mid_idx]
-    else:
-        a_list = list_of_integers[mid_idx + 1:]
-
-    return find_peak(a_list)
+    return lx[0]
