f53022891e57a435f64bb009cbb896be996557a9
Ehujuo chibueze <ehujuochibueze@gmail.com>
2023-04-27 18:17:48 +0100

    Task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# catch me if you can!
-curl -sL -X PUT -d User-Id=98 -H "You got me!" 0.0.0.0:5000/catch_me
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -s /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
